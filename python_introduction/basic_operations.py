05fffb3c5967560c46f8135a0e97b248617427ad
lemmyszn <lemmyxking17@gmail.com>
2024-06-18 10:09:00 +0300

    add script that assigns variables and perfoms simple arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Assign specific values to the variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results in a human-readable format
+print(f"The addition of {number1} and {number2} is: {addition}")
+print(f"The subtraction of {number1} from {number2} is: {subtraction}")
+print(f"The multiplication of {number1} and {number2} is: {multiplication}")
